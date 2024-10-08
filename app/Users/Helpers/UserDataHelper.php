f2391bf29ff81e5dcf90dd1898e981b1fbb4f7f0
Mohamed Karabawy <mohamedkarabawy@hotmail.com>
2024-10-09 02:48:15 +0300

    Ten Commit

--- app/Users/Helpers/UserDataHelper.php
+++ app/Users/Helpers/UserDataHelper.php
@@ -9,10 +9,11 @@ trait UserDataHelper
         foreach($class->permission['users'] as $permission_key => $permission_value)
         {
              //Chcek if permission allowed to perform manual assignments by the user otherwise default assignments stored in database
-             if($class->isUnique($permission_key, $action) && $class->isAllowed($class->current_user, $class->permission_collection, $permission_key, $user?->user_id))
-             {
+            if($class->isUnique($permission_key, $action) && $class->isAllowed($class->current_user, $permission_key, $class->permission_collection))
+            {
                 $permission_value === 'role_id'?  $request->has('role') && $user->$permission_value = $class->Role($request->role)->id : $request->has('is_activated') && $user->$permission_value = $request->$permission_value;
-             }
+            }
+
         }
     }
 }
