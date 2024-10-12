91d1e0651cbcd5d335e27e833a2aa772e1ef5607
Mohamed Karabawy <mohamedkarabawy@hotmail.com>
2024-10-12 15:42:26 +0300

    Batches update view

--- app/Batches/Helpers/GetBatches.php
+++ app/Batches/Helpers/GetBatches.php
@@ -9,10 +9,16 @@ trait GetBatches
     protected function getCollection($batches, $class)
     {
         $batches_collection = [];
-            
-        foreach($batches as $key => $g_batch)
+        
+        $data = [];
+
+        $active_index = 0;
+
+        $archive_index = 0;
+        
+        foreach($batches as $g_batch)
         {
-            $batches_collection[$key] = [
+            $batches_collection = [
                 'id' => $g_batch->id,
                 'batch_title' => $g_batch->batch_title,
                 'start_date' => Carbon::parse($g_batch->start_date)->format('Y-m-d'),
@@ -20,8 +26,10 @@ trait GetBatches
                 'status' => boolval($g_batch->is_active),
                 'num_classes' => count($g_batch->classes)
             ];
+
+            boolval($g_batch->is_active) === true ? $data['active'][$active_index++] = $batches_collection : $data['archive'][$archive_index++] = $batches_collection;
         }
         
-        return $batches_collection;
+        return $data;
     }
 }
