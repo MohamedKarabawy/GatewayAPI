d5cac409e0be09f4f8bd110255c444e37b05b413
Mohamed Karabawy <mohamedkarabawy@hotmail.com>
2024-10-16 03:26:27 +0300

    Classes

--- app/Trainees/Helpers/GetTraineeMeta.php
+++ app/Trainees/Helpers/GetTraineeMeta.php
@@ -42,17 +42,17 @@ trait GetTraineeMeta
                     $meta_collection[$meta->meta_key] = $meta->meta_value;
                 }
 
-                $class->isAllowed($class->current_user, 'view_trainers', $class->permission_collection, $trainee?->user_id) && $trainer_collection = ['trainer' => $trainee->user?->full_name];
+                $class->isAllowed($class->current_user, 'view_trainers', $class->permission_collection, $trainee?->user_id) && $trainer_collection = ['trainer' => $class->User($trainee->trainer_id)->full_name];
 
-                $class->isAllowed($class->current_user, 'view_own_trainers', $class->permission_collection, $trainee?->user_id) && $trainer_collection = ['trainer' => $trainee->user?->full_name];
+                $class->isAllowed($class->current_user, 'view_own_trainers', $class->permission_collection, $trainee?->user_id) && $trainer_collection = ['trainer' => $class->User($trainee->trainer_id)->full_name];
 
                 $class->isAllowed($class->current_user, 'view_levels', $class->permission_collection, $trainee?->user_id) && $level_collection = ['level' => $class?->GetGeneralMeta($trainee?->level)?->meta_value];
 
                 $class->isAllowed($class->current_user, 'view_own_levels', $class->permission_collection, $trainee?->user_id) && $level_collection = ['level' => $class?->GetGeneralMeta($trainee?->level)?->meta_value];
 
-                $class->isAllowed($class->current_user, 'view_follow_up', $class->permission_collection, $trainee?->user_id) && $follow_up_collection = ['follow_up' => $trainee?->user?->full_name];
+                $class->isAllowed($class->current_user, 'view_follow_up', $class->permission_collection, $trainee?->user_id) && $follow_up_collection = ['follow_up' => $class->User($trainee->follow_up)->full_name];
 
-                $class->isAllowed($class->current_user, 'view_own_follow_up', $class->permission_collection, $trainee?->user_id) && $follow_up_collection = ['follow_up' => $trainee?->user?->full_name];
+                $class->isAllowed($class->current_user, 'view_own_follow_up', $class->permission_collection, $trainee?->user_id) && $follow_up_collection = ['follow_up' => $class->User($trainee->follow_up)->full_name];
                 
                 $collection[$collection_index++] = [...$trainee_collection, ...$sub_collection, ...$trainer_collection, ...$level_collection, ...$follow_up_collection, ...$meta_collection, 'test_date' => $trainee?->test_date, 'moved_date' => Carbon::parse($trainee?->moved_date)->format("m/d/Y"),'created_at' => $trainee?->created_at, 'updated_at' => $trainee?->updated_at];
             }
