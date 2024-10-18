530a809af31e09e6b84e330f15a765d45106f6a6
Mohamed Karabawy <mohamedkarabawy@hotmail.com>
2024-10-18 22:19:36 +0300

    Solve some bugs

--- app/Batches/Helpers/ViewClassesHelper.php
+++ app/Batches/Helpers/ViewClassesHelper.php
@@ -10,13 +10,13 @@ trait ViewClassesHelper
     {
         $classes_data = [];
 
-        $class->CheckPermissionStatus($class->current_user, $class->permission_collection, 'view_classes') && $classes_data = $class->getCollection($classes->where('batch_id', $batch_id)->get(), $class);
+        $class->CheckPermissionStatus($class->current_user, $class->permission_collection, 'view_classes') && $classes_data = $class?->getCollection($classes?->where('batch_id', $batch_id)?->get(), $class);
 
         ($class->CheckPermissionStatus($class->current_user, $class->permission_collection, 'view_own_classes') && count($classes_data) === 0) &&
 
-        $classes_data = $class?->getCollection($classes->where('batch_id', $batch_id)->where('user_id', $class->current_user->id)->get(), $class);
+        $classes_data = $class?->getCollection($classes->where('batch_id', $batch_id)?->where('user_id', $class?->current_user?->id)?->get(), $class);
 
-        $num_classes = Batch::where('id', $batch_id)->first()->classes->count();
+        $num_classes = Batch::where('id', $batch_id)->first()?->classes?->count();
 
         $sub_message = $num_classes === 0 ?  response(['message' => "There's no classes available."], 200) : response(['message' => 'Unauthorized'], 401);
 
