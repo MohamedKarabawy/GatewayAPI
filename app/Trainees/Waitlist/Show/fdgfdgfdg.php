d98cbfd57ec590405bebfea7c576d81dba18a134
Mohamed Karabawy <mohamedkarabawy@hotmail.com>
2024-10-20 19:00:49 +0300

    Discovery

--- app/Trainees/Waitlist/Show/fdgfdgfdg.php
+++ app/Trainees/Waitlist/Show/fdgfdgfdg.php
@@ -7,7 +7,7 @@ use App\Models\Trainee;
 use App\Models\ClassMeta;
 use Illuminate\Support\Facades\Gate;
 
-class ViewCLassesTime
+class fdgfdgfdg
 {
     public function __construct(?Trainee $trainee)
     {
