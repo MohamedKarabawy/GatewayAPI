f2f4bd1e8532da9e89be7b1515907d9a87056fc8
Mohamed Karabawy <mohamedkarabawy@hotmail.com>
2024-10-21 17:19:08 +0300

    Discovery

--- app/Trainees/Waitlist/Show/ViewClasses.php
+++ app/Trainees/Waitlist/Show/ViewClasses.php
@@ -6,11 +6,16 @@ use Exception;
 use App\Models\Batch;
 use App\Models\Classes;
 use App\Models\Trainee;
+use App\Traits\GetUser;
+use App\Models\TraineeClass;
+use App\Traits\GetClassMeta;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 
 class ViewClasses
 {
+    use GetClassMeta, GetUser;
+    
     public function __construct(?Trainee $trainee)
     {
         Gate::authorize('assignClass', $trainee);
@@ -24,31 +29,36 @@ class ViewClasses
 
             $classes = [];
 
-            (!$request->filled('class_type') && !$request->filled('level_id') && !$request->filled('time_id')) && $classes = $class?->where('batch_id', $current_batch?->id)?->get();
+            $filter_elements = ['class_type', 'level', 'time_slot', 'trainer_id'];
 
-            ($request->filled('class_type') && $request->filled('level_id') && $request->filled('time_id')) && $classes = $class->where('batch_id', $current_batch->id)->where('class_type', $request->class_type)->where('level', $request->level_id)->where('time_slot', $request->time_id)->get();
-        
-            ($request->filled('class_type') && $request->filled('time_id') && !$request->filled('level_id')) && $classes = $class->where('batch_id', $current_batch->id)->where('class_type', $request->class_type)->where('time_slot', $request->time_id)->get();
+            $classes = $class->where('batch_id', $current_batch?->id);
 
-            ($request->filled('class_type') && $request->filled('level_id') && !$request->filled('time_id')) && $classes = $class->where('batch_id', $current_batch->id)->where('class_type', $request->class_type)->where('level', $request->level_id)->get();
-            
-            ($request->filled('level_id') && $request->filled('time_id') && !$request->filled('class_type')) && $classes = $class->where('batch_id', $current_batch->id)->where('level', $request->level_id)->where('time_slot', $request->time_id)->get();
-            
-            ($request->filled('class_type') && !$request->filled('time_id') && !$request->filled('level_id')) && $classes = $class->where('batch_id', $current_batch->id)->where('class_type', $request->class_type)->get();
+            $current_classes = $classes;
 
-            ($request->filled('level_id') && !$request->filled('time_id') && !$request->filled('class_type')) && $classes = $class->where('batch_id', $current_batch->id)->where('level', $request->level_id)->get();
-            
-            ($request->filled('time_id') && !$request->filled('class_type') && !$request->filled('level_id')) && $classes = $class->where('batch_id', $current_batch->id)->where('time_slot', $request->time_id)->get();
-    
+            foreach($filter_elements as $filter_element)
+            {
+                $request->filled($filter_element) && $current_classes = $current_classes->where($filter_element, $request->$filter_element);
+            }
+
+            $classes = $current_classes->get();
+
             $classes_collection = [];
 
             foreach((object) $classes as $key => $t_class)
             {
                 $classes_collection[$key] = [
                     'id' => $t_class?->id,
-                    'name' => $t_class?->class_name
+                    'trainer' => $this_class->User($t_class->user_id)->first()?->full_name,
+                    'class_name' => $t_class?->class_name,
+                    'class_type' => $t_class?->class_type,
+                    'gate' => $this_class?->meta($t_class->gate)?->first()?->meta_value,
+                    'time_slot' => $this_class->meta($t_class->time_slot)->first()?->meta_value,
+                    'level' => $this_class->meta($t_class->level)->first()?->meta_value,
+                    'num_of_trainees' => TraineeClass::where('class_id', $t_class->id)->count(),
+                    'num_of_confirmation' => TraineeClass::where('class_id', $t_class->id)->where('confirmation', true)->count()
                 ];
             }
+
             
             return response($classes_collection, 200);
         }
--- app/Trainees/Waitlist/Show/ViewClassesLevels.php
+++ app/Trainees/Waitlist/Show/ViewClassesLevels.php
@@ -20,7 +20,6 @@ class ViewClassesLevels
     {
         try
         {
-
             $levels = $level->where('meta_key', $this->collection_key)->get();
 
             $levels_collection = [];
