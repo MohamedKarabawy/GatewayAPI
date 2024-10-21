1010666653cdd8d59659067759a993ee35ece930
Mohamed Karabawy <mohamedkarabawy@hotmail.com>
2024-10-21 20:48:45 +0300

    Discovery

--- app/Batches/Classes/Class/Move/SwitchClass.php
+++ app/Batches/Classes/Class/Move/SwitchClass.php
@@ -5,6 +5,7 @@ namespace App\Batches\Classes\Class\Move;
 
 use Exception;
 use App\Models\Classes;
+use App\Models\Attendance;
 use App\Models\TraineeClass;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
@@ -22,16 +23,18 @@ class SwitchClass
         {
             $is_exists = $trainee_class->where("class_id", $request->class_id)->where("trainee_id", $trainee_id)->exists();
             
-            if(!$is_exists)
+            if(!$is_exists && $request->filled('old_class'))
             {
                 $trainee_class->where("trainee_id", $trainee_id)->update([
                     "class_id" => $request->class_id
                 ]);
+
+                Attendance::where('class_id', $request->old_class)->where("trainee_id", $trainee_id)->update(['class_id', $request->class_id]);
                 
                 return response(['message' => "Class switched successfully."], 200);
             }
 
-            return response(['message' => "Cannot switch class. Class not exists."], 400);
+            return response(['message' => "Cannot switch class. Class not exists or class id is wrong."], 400);
         }
         catch (Exception $e)
         {
